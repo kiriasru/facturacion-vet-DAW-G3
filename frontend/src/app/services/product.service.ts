5bc7dabe4ff5fb7174c6742f220b2b06f6fc0d2b
kiriasru <kiriasru@gmail.com>
2025-12-14 21:30:57 -0600

    Pantalla de login

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,1 +1,16 @@
-// Kevin
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.get(`${this.apiUrl}/productos`, { headers });
+  }
+}
